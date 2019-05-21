1ce39691619c0ceef99da3cdc87b20bc0f1b444e
derrickwu1984 <derrickwu@sina.com>
2019-05-21 15:11:43 +0800

    fuck readexcel

--- readExcel.py
+++ readExcel.py
@@ -29,7 +29,6 @@ def read_excel():
             if (sheet_row_values[0]=='输出'):
                 input_end_index = row
         # 第二次循环，确认每条记录是输入项还是输出项
-        #temp结果集中已经过滤掉输入、输出
         for row in range(sheet.nrows)[start_index:]:
             sheet_row_values = sheet.row_values(row)
             if (sheet_row_values[0] != '输入' and sheet_row_values[0] != '输出' and row > int(input_start_index) and row < int(input_end_index)):
@@ -48,7 +47,7 @@ def read_excel():
 def insert_db(values):
     print (values)
     line=[]
-    trs_code =values[0]
+    trs_code_id =values[0]
     trs_name = values[1]
     fuc_desc = values[2]
     eng_name = values[3]
@@ -65,17 +64,13 @@ def insert_db(values):
     db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, port=3306,charset="utf8")
     cur = db.cursor()
     insert_sql = """
-    insert into api_api(id,trs_code,trs_name,fuc_desc,eng_name,chinese_name,data_type,required,remark,flag)
+    insert into interface_details(id,trs_code_id,trs_name,fuc_desc,eng_name,chinese_name,data_type,required,remark,flag)
       values (%s,%s, %s, %s,%s,%s,%s, %s,%s,%s)
         """
     cur.execute(insert_sql,
-                (str(uuid.uuid1()),trs_code, trs_name,fuc_desc,eng_name,chinese_name,data_type, required,remark,flag)
+                (str(uuid.uuid1()),trs_code_id, trs_name,fuc_desc,eng_name,chinese_name,data_type, required,remark,flag)
                 )
     db.commit();
-    # line=[trs_code,trs_name,func_des,eng_name,chinese_name,data_type,required,remark,flag]
-    # print (line)
-    # db.insert_api(line)
-
 
 if __name__ == '__main__':
     read_excel()
